90af256041bf4d3a1140a67864de48e56d5a699a
Kuldip Sirpali <kuldipsirpali779@gmail.com>
2025-07-18 19:52:42 +0545

    add project images

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ import {
 } from "react-icons/si";
 import { DiRedis } from "react-icons/di";
 import { CiShare1 } from "react-icons/ci";
+import Link from "next/link";
 
 export default function Home() {
   const projects = [
@@ -26,6 +27,8 @@ export default function Home() {
       githubLink: "https://github.com/Kuldip-Sirpali/Ecommerce_Mern",
       websiteLink: "https://ecomartia.vercel.app/",
       stack: "React / NodeJs / Tailwind / MongoDB / Google OAuth",
+      image:
+        "https://res.cloudinary.com/djlajb0wj/image/upload/v1752846485/e-m_llsbfb.png",
     },
     {
       title: "Social Media Platform",
@@ -33,34 +36,46 @@ export default function Home() {
       // githubLink: "https://github.com/Kuldip-Sirpali/",
       websiteLink: "https://hubbell.vercel.app/",
       stack: "React / Tailwind / NodeJs / MongoDb",
+      image:
+        "https://res.cloudinary.com/djlajb0wj/image/upload/v1752846485/s-m_hqmqyk.png",
     },
     {
-      title: "Audio Book Summary App",
+      title: "Audio Streaming platform App",
       description: "Get curated audio book summaries in one place.",
       // githubLink: "https://github.com/Kuldip-Sirpali/",
       websiteLink: "https://dhawni.vercel.app/",
       stack: "React / Express / MongoDB /Google OAuth",
+      image:
+        "https://res.cloudinary.com/djlajb0wj/image/upload/v1752846485/a-p_p7keei.png",
     },
+
     {
-      title: "Learning Management System",
-      description: "Manage and sell your courses easily.",
-      // githubLink: "https://github.com/Kuldip-Sirpali/",
-      websiteLink: "https://ecomartia.vercel.app/",
-      stack: "Next.js / Tailwind / Prisma / Clerk",
-    },
-       {
       title: "Netflix Clone - A video streaming platform",
       description: "A frontend ui clone of netflix using venilla javascript. ",
       githubLink: "https://github.com/Kuldip-Sirpali/video-app-frontend",
       websiteLink: "https://kuldip-sirpali.github.io/video-app-frontend",
       stack: "HTML / CSS / JS",
+      image:
+        "https://res.cloudinary.com/djlajb0wj/image/upload/v1752846485/n-x_kzcuol.png",
     },
-     {
+    {
       title: "Linkedin UI ",
-      description: "Frontend of Linkedin with single page feature in javascript",
+      description:
+        "Frontend of Linkedin with single page feature in javascript",
       githubLink: "https://github.com/Kuldip-Sirpali/Linkedin-Clone",
       websiteLink: "https://kuldip-sirpali.github.io/Linkedin-Clone/",
       stack: "HTML / CSS / JS",
+      image:
+        "https://res.cloudinary.com/djlajb0wj/image/upload/v1752846485/l-in_tski7k.png",
+    },
+    {
+      title: "Xpaces-Experience blackhole",
+      description: "Simple animated blackhole virtual reality",
+      // githubLink: "https://github.com/Kuldip-Sirpali/",
+      websiteLink: "https://xpaces.netlify.app/",
+      stack: "HTML / CSS / JS",
+      image:
+        "https://res.cloudinary.com/djlajb0wj/image/upload/v1752847252/b-h_h2m7xr.png",
     },
   ];
 
@@ -83,20 +98,39 @@ export default function Home() {
   ];
 
   const toolsAndTechStacks = [
-    { name: "React", icon: SiReact },
-    { name: "Next.js", icon: RiNextjsFill },
-    { name: "Express", icon: SiExpress },
-    { name: "React Router", icon: SiReactrouter },
-    { name: "JavaScript", icon: SiJavascript },
-    { name: "TypeScript", icon: SiTypescript },
-    { name: "Node.js", icon: SiNodedotjs },
-    { name: "Git", icon: SiGit },
-    { name: "Redis", icon: DiRedis },
-    { name: "Tailwind CSS", icon: SiTailwindcss },
-    { name: "Clerk", icon: SiClerk },
-    { name: "Redux Toolkit", icon: SiRedux },
+    { name: "React", icon: SiReact, link: "https://react.dev/" },
+    { name: "Next.js", icon: RiNextjsFill, link: "https://nextjs.org/" },
+    { name: "Express", icon: SiExpress, link: "https://expressjs.com/" },
+    {
+      name: "React Router",
+      icon: SiReactrouter,
+      link: "https://reactrouter.com/",
+    },
+    {
+      name: "JavaScript",
+      icon: SiJavascript,
+      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    },
+    {
+      name: "TypeScript",
+      icon: SiTypescript,
+      link: "https://www.typescriptlang.org/",
+    },
+    { name: "Node.js", icon: SiNodedotjs, link: "https://nodejs.org/en" },
+    { name: "Git", icon: SiGit, link: "https://git-scm.com/" },
+    { name: "Redis", icon: DiRedis, link: "https://redis.io/" },
+    {
+      name: "Tailwind CSS",
+      icon: SiTailwindcss,
+      link: "https://tailwindcss.com/",
+    },
+    { name: "Clerk", icon: SiClerk, link: "https://clerk.com/" },
+    {
+      name: "Redux Toolkit",
+      icon: SiRedux,
+      link: "https://redux-toolkit.js.org/",
+    },
   ];
-
   return (
     <motion.main
       className="relative min-h-screen bg-[#0f0f0f] text-white overflow-hidden py-12"
@@ -155,60 +189,69 @@ export default function Home() {
           </div>
         </motion.section>
 
-       
-<motion.section
-  className="space-y-4"
-  initial={{ opacity: 0, y: 20 }}
-  animate={{ opacity: 1, y: 0 }}
-  transition={{ delay: 0.4 }}
->
-  <h2 className="text-2xl font-semibold text-white ">Projects</h2>
+        <motion.section
+          className="space-y-4"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.4 }}
+        >
+          <h2 className="text-2xl font-semibold text-white ">Projects</h2>
 
-  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-    {projects.map((project, index) => (
-      <motion.div
-        key={index}
-        className="flex flex-col justify-between p-4 border border-white/10 rounded-xl bg-white/2 backdrop-blur-md hover:scale-[1.01] hover:shadow-xl transition-all"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.5 + index * 0.1 }}
-      >
-        {/* Project title, description, and stack */}
-        <div>
-          <h3 className="font-semibold text-white text-sm">
-            {project.title}
-          </h3>
-          <p className="text-gray-300 text-xs mt-1">{project.description}</p>
-          <p className="text-[11px] text-gray-400 mt-2 italic">
-            {project.stack}
-          </p>
-        </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {projects.map((project, index) => (
+              <motion.div
+                key={index}
+                className="flex flex-col justify-between p-4 border border-white/10 rounded-xl bg-white/2 backdrop-blur-md hover:scale-[1.01] hover:shadow-xl transition-all"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.5 + index * 0.1 }}
+              >
+                {/* Project title, description, and stack */}
+                <div>
+                  {project.image && (
+                    <img
+                      src={project.image}
+                      alt={project.title}
+                      loading="lazy"
+                      className="w-full h-32 object-cover rounded-lg mb-2"
+                    />
+                  )}
+                  <h3 className="font-semibold text-white text-sm">
+                    {project.title}
+                  </h3>
+                  <p className="text-gray-300 text-xs mt-1">
+                    {project.description}
+                  </p>
+                  <p className="text-[11px] text-gray-400 mt-2 italic">
+                    {project.stack}
+                  </p>
+                </div>
 
-        {/* Bottom aligned buttons */}
-        <div className="mt-4 flex gap-2 text-xs text-gray-300">
-          <a
-            href={project.websiteLink}
-            className="flex items-center gap-1 px-2 py-0.5 rounded hover:bg-white/10"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            View <CiShare1 />
-          </a>
-          {project?.githubLink && (
-            <a
-              href={project.githubLink}
-              className="flex items-center gap-1 px-2 py-0.5 rounded hover:bg-white/10"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub <CiShare1 />
-            </a>
-          )}
-        </div>
-      </motion.div>
-    ))}
-  </div>
-</motion.section>
+                {/* Bottom aligned buttons */}
+                <div className="mt-4 flex gap-2 text-xs text-gray-300">
+                  <a
+                    href={project.websiteLink}
+                    className="flex items-center gap-1 px-2 py-0.5 rounded hover:bg-white/10"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View <CiShare1 />
+                  </a>
+                  {project?.githubLink && (
+                    <a
+                      href={project.githubLink}
+                      className="flex items-center gap-1 px-2 py-0.5 rounded hover:bg-white/10"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      GitHub <CiShare1 />
+                    </a>
+                  )}
+                </div>
+              </motion.div>
+            ))}
+          </div>
+        </motion.section>
 
         <motion.section
           className="space-y-2"
@@ -218,12 +261,19 @@ export default function Home() {
         >
           <h3 className="text-2xl font-semibold">Tools & Stack</h3>
           <div className="grid grid-cols-2 sm:grid-cols-4 gap-2 text-xs text-white/80">
-            {toolsAndTechStacks.map(({ icon: Icon, name }, index) => (
+            {toolsAndTechStacks.map(({ icon: Icon, name, link }, index) => (
               <div
                 className="flex items-center gap-2 hover:text-white transition"
                 key={index}
               >
-                <Icon /> {name}
+                <Link
+                  href={link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center gap-1"
+                >
+                  <Icon /> {name}
+                </Link>
               </div>
             ))}
           </div>
